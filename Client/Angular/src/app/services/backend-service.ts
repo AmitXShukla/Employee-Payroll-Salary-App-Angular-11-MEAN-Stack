62b32e05a7256f72e5699bc64fa1086d73e3f6a6
Amit Shukla <ashuklax@gmail.com>
2019-02-27 22:58:36 -0800

    Update Angular 7
    Update Angular 7

--- Client/Angular/src/app/services/backend-service.ts
+++ Client/Angular/src/app/services/backend-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/observable/of';
 
 @Injectable({
